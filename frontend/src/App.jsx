4a67ddf7d1ced865e4731aa0127dcfa9808d9079
Xavier Vitualla <xavierbob48@gmail.com>
2026-03-07 00:21:18 +0800

    Added frontend with header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="bg-[#FDFBD4] h-svh">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="heading text-center p-4 bg-[#D4AF37] font-extrabold">
+        NOTES, ONLY FOR YOU
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
